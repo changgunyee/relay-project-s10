4d52fed555de44f08d0bd6b4a6aed540fab50301
changgunyee <minyoung403@naver.com>
2019-07-26 17:27:05 +0900

    프론트 백 연결

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const compare = require('./compare.js');
+const cors = require('cors');
+
+app.use(cors());
 // respond with "hello world" when a GET request is made to the homepage
 app.post('/', function (req, res) {
     const result = compare(req);
